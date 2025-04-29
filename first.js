4819dfcd781a3256b938f62db381395977db44f5
ankit-yadav90 <ankityadav73699@gmail.com>
2025-04-29 11:30:38 +0530

    add new class

--- first.js
+++ first.js
@@ -1,10 +1,15 @@
+//qs 1
 let newbtn = document.createElement("button");
 newbtn.innerText = "click me"
-console.log(newbtn);
 
 newbtn.style.backgroundColor = "red";
 newbtn.style.color = "white";
 
 document.querySelector("body").prepend(newbtn);
+
+// Qs 2
+let para = document.querySelector("p");
+
+
 
 
